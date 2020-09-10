ba405568757fcc19cd3b1dabe3bcf0df86ec125a
Frank UN <frankuchenna30@gmail.com>
2020-09-10 12:13:50 +0100

    modified url and added signup template

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -12,14 +12,14 @@ from accounts.views.student_views import (StudentSignUpView,
 
 
 urlpatterns=[
-	path('signup/', SignUpView.as_view(), name='signup'),
-	path('signup/as_student/', StudentSignUpView.as_view(), name='std_signup'),
+	path('signup', SignUpView.as_view(), name='signup'),
+	path('signup/as_student', StudentSignUpView.as_view(), name='std_signup'),
 	# path('signup/as_teacher/', TeacherSignUpView.as_view(), name='teacher_signup'),
-    path('email_activation/<uidb64>/<token>/', ActivateAccount.as_view(), name='email_activation'),
-	path('login/', LogInView.as_view(), name='login'),
-	path('logout/', LogOutView.as_view(), name='logout'),
+    path('email_activation/<uidb64>/<token>', ActivateAccount.as_view(), name='email_activation'),
+	path('login', LogInView.as_view(), name='login'),
+	path('logout', LogOutView.as_view(), name='logout'),
 	path('student/<int:pk>/student_update_profile', StudentUpdateProfileView.as_view(), name='student_update_profile'),
 	# path('teacher/<int:pk>/teacher_update_profile', TeacherUpdateProfileView.as_view(), name='teacher_update_profile'),
-	path('student/<int:pk>/', StudentProfileView.as_view(), name='teacher_profile'),
+	path('student/<int:pk>', StudentProfileView.as_view(), name='teacher_profile'),
 	# path('teacher/<int:pk>/', TeacherProfileView.as_view(), name='student_profile'),
 ]
